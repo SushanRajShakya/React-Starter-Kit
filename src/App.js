d510c6b9a52c546bbb4fd5c87b5f69dc0f9e9bcb
Sushan Raj Shakya <sushanrajshakya@lftechnology.com>
2019-08-28 10:12:06 -0400

    master #time 5m Added header styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// Remove this CSS once you start using the starter kit
+const headerStyle = {
+  textAlign: 'center',
+  width: '100%',
+  background: '#bcbcbc',
+};
 
 /**
  * Main App Component.
  */
 const App = () => {
   return (
-    <div className="App">
-      {' '}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>{' '}
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React{' '}
-        </a>{' '}
-      </header>
+    <div>
+      <h1 style={headerStyle}>React Starter Kit</h1>
     </div>
   );
 };
